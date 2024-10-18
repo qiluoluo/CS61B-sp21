d82d813b7cda6b51e4243758dbbc3fc46640b3fe
Ki1ro <38156264+qiluoluo@users.noreply.github.com>
2024-10-18 14:42:16 +0800

    Finished Lab 1! Collatz works!

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,13 +5,13 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n % 2 == 0) {
-            n = n / 2;
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
         } else {
-            n = 3 * n + 1;
+            return n * 2;
         }
-        return n;
-    }
 
     public static void main(String[] args) {
         int n = 5;
